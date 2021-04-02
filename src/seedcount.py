7916d8d5290324af21ff403885cca07dcdf3ea63
Alice Sturm <alice.sturm@gmail.com>
2021-04-01 21:16:56 -0400

    testing plots

--- src/seedcount.py
+++ src/seedcount.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import random
 import altair as alt
-
+from loguru import logger
 
 #To run, paste into terminal: streamlit run seedcount.py
 
@@ -112,15 +112,14 @@ source = metercalc
 highlight = alt.selection(type='single',
                           fields=['species', 'common_name'], empty="all")
 #color
-color = alt.condition(highlight,
-                      alt.Color('species:N', scale=alt.Scale(scheme='spectral')),
-                      alt.value('lightgray'))
+#color = alt.condition(highlight,
+                      #alt.Color('bloom_color', scale=None),
+                      #alt.value('lightgray'))
 
 #define the density plot
 visualizeseeds = alt.Chart(source).mark_point(filled=True, size=100).encode(
     x=alt.X('x', axis=alt.Axis(title='1 meter')),
     y=alt.Y('y', axis=alt.Axis(title='1 meter')),
-    color=alt.Color('bloom_color', legend=None),
     tooltip=['species:N', 'common_name:N'],
 ).add_selection(
     highlight
@@ -138,3 +137,54 @@ seasonaldistribution = alt.Chart(source).mark_bar(size=100, color='Green').encod
 ).properties(width=500, height=300)
 #call seasonality chart with streamlit
 st.altair_chart(seasonaldistribution)    
+choices = seeddf.loc[seeddf['forb'] == 1]
+
+species=[]
+season= []
+bcolor= []
+
+for index, row in choices.iterrows():
+        for i in range (0, row['plants_per_meter']):
+            bcolor.append(row["bloom_color"])
+            species.append(index)
+            if row["spring"]==1 and row["summer"]==0 and row["autumn"]==0:
+                season.append("spring")
+            elif row["summer"]==1 and row ["spring"]==0 and row ["autumn"]==0:
+                season.append("summer")
+            elif row["autumn"]==1 and row ["spring"]==0 and row ["summer"]==0:
+                season.append("autumn")
+            elif row["spring"]==1 and row ["summer"]==1 and row["autumn"]==1:
+                season.append("spring")
+                season.append("summer")
+                season.append("autumn")
+                bcolor.append(row["bloom_color"])
+                bcolor.append(row["bloom_color"])
+                bcolor.append(row["bloom_color"]) 
+                species.append(index)
+                species.append(index)
+                species.append(index)
+            elif row["spring"]==1 and row ["summer"]==1 and row["autumn"]==0:
+                season.append("spring")
+                season.append("summer")
+                bcolor.append(row["bloom_color"])
+                bcolor.append(row["bloom_color"])
+                species.append(index) 
+                species.append(index)
+            elif row["spring"]==0 and row ["summer"]==1 and row["autumn"]==1:
+                season.append("summer")
+                season.append("autumn")
+                bcolor.append(row["bloom_color"])
+                bcolor.append(row["bloom_color"])
+                species.append(index) 
+                species.append(index)
+    
+source = pd.DataFrame((species,season,bcolor), index=None).T
+source["bcolor"]=source["bcolor"].astype('category')
+
+seasonaldistribution2 = alt.Chart(source).mark_bar(size=100).encode(
+    x="season",
+    y="species",
+    color="bcolor"
+).properties(width=500, height=300)
+
+st.altair_chart(seasonaldistribution2)   
